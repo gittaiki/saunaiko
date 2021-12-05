28a121888d06a2837ae018f61b71efd09aaf8179
gittaiki <0215.taiki@gmail.com>
2021-12-05 15:33:28 +0900

    Add: Bootstrapの導入

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,6 +6,10 @@
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
+
+import "bootstrap"
+import "../stylesheets/application"
+
 require("channels")
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
+  root 'static_pages#top'
   post 'callback', to: 'line_bots#callback'
 end
