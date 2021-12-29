0a9df4cfbef67d6f5cee5f9e0ecf8a05724f6a80
gittaiki <0215.taiki@gmail.com>
2021-12-29 11:26:43 +0900

    Add: videosコントローラ作成

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,3 +1,12 @@
 class VideosController < ApplicationController
-  def index; end
+  def index
+    @videos = case params[:level]
+              when 'basic'
+                Video.basic
+              when 'advanced'
+                Video.advanced
+              else
+                Video.entry
+              end
+  end
 end
