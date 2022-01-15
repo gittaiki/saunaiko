0a13630bf332d13b232da761019a652b5c07aab6
gittaiki <0215.taiki@gmail.com>
2022-01-15 20:35:49 +0900

    Fix: 動画データ追加とチャンネル別に整理#27

--- db/fixtures/video.rb
+++ db/fixtures/video.rb
@@ -1,51 +1,52 @@
 Video.seed(:youtube_id,
+  # 名古屋PR
   {
-    youtube_id: 'DTL3DShbKhc',
-    address: '愛知県 岡崎市 庄司田１丁目１４-１４',
-    sauna: 'おかざき楽の湯'
+    youtube_id: '7lPM4Y6Vsbk',
+    address: '愛知県 名古屋市 中区栄３丁目３-１３-１２',
+    sauna: 'ウェルビー栄'
   },
+  # 松野井雅
   {
-    youtube_id: 'yiUP6-OQbqo',
-    address: '静岡県 静岡市 駿河区敷地2丁目25-1',
-    sauna: 'サウナしきじ'
+    youtube_id: '2VveTXPKtVU',
+    address: '北海道 札幌市中央区 南3条西2丁目14番地 ニコービル',
+    sauna: 'ニコーリフレ SAPPORO'
   },
+  # しもふり
   {
-    youtube_id: 'CIXSwK2Ap0A',
-    address: '北海道 川上郡標茶町 塘路原野北７線49番43',
-    sauna: 'THE GEEK'
+    youtube_id: 'OOZjlb8wV5o',
+    address: '山梨県南都留郡鳴沢村字飯塚1708-1',
+    sauna: 'abrAsus hotel Fuji'
   },
-  {
-    youtube_id: '1MkEhopnmWE',
-    address: '北海道 標津郡中標津町 養老牛５１８',
-    sauna: '湯宿だいいち'
-  },
-  {
-    youtube_id: 'w1pWFesqPvA',
-    address: '静岡県 静岡市 駿河区敷地2丁目25-1',
-    sauna: 'サウナしきじ'
-  },
-  {
-    youtube_id: '58OZ5pBhKLo',
-    address: '千葉県 船橋市 本町５丁目１８-１',
-    sauna: 'ジートピア'
-  },
-  {
-    youtube_id: 'ozWEydEg3r0',
-    address: '東京都 台東区 日本堤１丁目４-５',
-    sauna: '天然温泉 湯どんぶり栄湯'
-  },
-  {
-    youtube_id: 'm4HBXCoLSR0',
-    address: '東京都 豊島区 池袋2丁目7-7 6Fフロント入口（3階～10階）',
-    sauna: 'かるまる 池袋店'
-  },
+  # ロンブー
   {
     youtube_id: 'iTSsv7AZ8dw',
     address: '東京都 豊島区 池袋2丁目7-7 6Fフロント入口（3階～10階）',
     sauna: 'かるまる 池袋店'
   },
   {
+    youtube_id: 'wATQAYmcYEM',
+    address: '東京都 豊島区 池袋2丁目7-7 6Fフロント入口（3階～10階）',
+    sauna: 'かるまる 池袋店'
+  },
+  # マグ万平
+  {
+    youtube_id: 'pp5-dedlv-k',
+    address: '長崎県 佐世保市 塩浜町6-15',
+    sauna: 'サウナサン'
+  },
+  {
+    youtube_id: 'k9n1BQWVmFU',
+    address: '神奈川県 横浜市 西区高島２丁目１９-１２ スカイビル14階',
+    sauna: 'スカイスパYOKOHAMA'
+  },
+  # アルピー
+  {
     youtube_id: '_sgLjNF7ROA',
+    address: '東京都 新宿区 歌舞伎町１丁目１−２',
+    sauna: '新宿天然温泉 テルマー湯'
+  },
+  {
+    youtube_id: 'K4VK6PV7kFA',
     address: '東京都 新宿区 歌舞伎町１丁目１−２',
     sauna: '新宿天然温泉 テルマー湯'
   },
@@ -55,19 +56,62 @@ Video.seed(:youtube_id,
     sauna: 'ファンタジーサウナandスパおふろの国'
   },
   {
-    youtube_id: '3yTmz3DR9d0',
-    address: '東京都 台東区 上野７丁目２−１６',
-    sauna: '北欧'
+    youtube_id: 'ctB-eyztcFo',
+    address: '神奈川県 川崎市 川崎区鋼管通3丁目1-2',
+    sauna: '朝日湯源泉ゆいる'
   },
+  # TKO木本
   {
     youtube_id: 'gSVETgZWOmE',
     address: '千葉県 館山市 波左間1063-2',
     sauna: 'Sea Sauna Shack'
   },
   {
-    youtube_id: 'OOZjlb8wV5o',
-    address: '山梨県南都留郡鳴沢村字飯塚1708-1',
-    sauna: 'abrAsus hotel Fuji'
+    youtube_id: 'fZFRl2jXMHY',
+    address: '東京都 台東区 上野７丁目２−１６',
+    sauna: '北欧'
+  },
+  {
+    youtube_id: 'Qx5F1CD5GoY',
+    address: '埼玉県 草加市 北谷２丁目２３－２３',
+    sauna: '湯乃泉 草加健康センター'
+  },
+  {
+    youtube_id: '7q9EO-AeUYY',
+    address: '大阪府 東大阪市 長堂３丁目４−２１',
+    sauna: 'なにわ健康ランド 湯〜トピア'
+  },
+  # サウナどっとこむ
+  {
+    youtube_id: 'rdP-7HyS3Zo',
+    address: '愛知県 知立市 宝町塩掻５８',
+    sauna: 'サウナイーグル'
+  },
+  {
+    youtube_id: 'L1Gl273wI4Y',
+    address: '神奈川県 横浜市 港北区樽町3-7-61',
+    sauna: '綱島源泉 湯けむりの庄'
+  },
+  {
+    youtube_id: 'MEq9AKHlbuM',
+    address: '東京都 杉並区 上荻１丁目１０-１０',
+    sauna: '東京荻窪天然温泉 なごみの湯'
+  },
+  {
+    youtube_id: 'Al6oSnTIr-s',
+    address: '愛知県 名古屋市 千種区今池５丁目２５',
+    sauna: 'ウェルビー今池'
+  },
+  # サウナチャンネル
+  {
+    youtube_id: '3o1OtZmn438',
+    address: '神奈川県 横浜市 鶴見区下末吉２丁目２５-２３',
+    sauna: 'ファンタジーサウナandスパおふろの国'
+  },
+  {
+    youtube_id: 'YqbNN1v7Ss0',
+    address: '熊本県 熊本市 中央区本荘町７２２',
+    sauna: 'サウナと天然温泉 湯らっくす'
   },
   {
     youtube_id: '-AEkLQJ6ORU',
@@ -75,18 +119,33 @@ Video.seed(:youtube_id,
     sauna: '朝日湯源泉ゆいる'
   },
   {
-    youtube_id: 'EEUAqusfu68',
-    address: '東京都 中野区 東中野５丁目２９−１２',
-    sauna: '松本湯'
+    youtube_id: 'RAn7YBJ8f-M',
+    address: '北海道 札幌市中央区 南3条西2丁目14番地 ニコービル',
+    sauna: 'ニコーリフレ SAPPORO'
   },
   {
-    youtube_id: '-jrB0NIswss',
-    address: '千葉県 千葉市 美浜区美浜26 JFA夢フィールド内',
-    sauna: 'JFA夢フィールド 幕張温泉 湯楽の里'
+    youtube_id: 'Yim0yk0wpww',
+    address: '北海道 札幌市南区 小金湯25',
+    sauna: '小金湯温泉 湯元 小金湯'
   },
   {
-    youtube_id: 'hvPfgP4PYnk',
-    address: '東京都 渋谷区 東2丁目19-9 金山ビル',
-    sauna: '改良湯'
+    youtube_id: 'tdXfu_NZZuk',
+    address: '熊本県 上益城郡嘉島町 上島 2232',
+    sauna: '嘉島湯元水春'
+  },
+  {
+    youtube_id: 'A_cF91FkG2c',
+    address: '岩手県 八幡平市 安比高原１１７-１７',
+    sauna: 'ANAクラウンプラザリゾート安比高原'
+  },
+  {
+    youtube_id: 'oYrsGdEq8QQ',
+    address: '福島県 南相馬市 原町区高見町2-86-1',
+    sauna: 'ビジネスホテル高見'
+  },
+  {
+    youtube_id: 'vu5smjc6D0k',
+    address: '千葉県 館山市 波左間1063-2',
+    sauna: 'Sea Sauna Shack'
   }
 )
