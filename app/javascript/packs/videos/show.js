157832db6417c5b3b516cb27364bf9c02aee82a2
gittaiki <0215.taiki@gmail.com>
2022-01-16 19:02:35 +0900

    Improve: 動画をループ再生に設定#30

---
+++ app/javascript/packs/videos/show.js
@@ -0,0 +1,36 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const video = document.getElementById('video');
+  const youtube_id = video.dataset.youtube;
+
+  const tag = document.createElement('script');
+  tag.src = "https://www.youtube.com/iframe_api";
+  const firstScriptTag = document.getElementsByTagName('script')[0];
+  firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+  let player;
+  window.onYouTubeIframeAPIReady = function() {
+    player = new YT.Player('player', {
+      width: '560',
+      height: '315',
+      videoId: youtube_id,
+      events: {
+        // api呼び出す準備ができると起動
+        'onReady': function(evt) {
+          evt.target.mute();
+          evt.target.playVideo();
+        },
+        // プレーヤーの状態が変わると起動
+        'onStateChange': function(evt) {
+          switch (evt.data) {
+            // 動画をループ再生
+            case YT.PlayerState.ENDED:
+              evt.target.playVideo();
+              break;
+          }
+        }
+      },
+      playerVars: {
+        playsinline: 1,
+      }
+    });
+  }
+})
