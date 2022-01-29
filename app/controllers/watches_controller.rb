7bf2a51facfc4d4a4005b9e0c512386a543f6bb4
gittaiki <0215.taiki@gmail.com>
2022-01-29 23:33:59 +0900

    Add: 視聴ボタンのajax化#33

--- app/controllers/watches_controller.rb
+++ app/controllers/watches_controller.rb
@@ -2,12 +2,12 @@ class WatchesController < ApplicationController
   def create
     @video = Video.find(params[:video_id])
     current_user.watch(@video)
-    redirect_back fallback_location: root_path
+    # redirect_back fallback_location: root_path
   end
 
   def destroy
     @video = current_user.watches.find(params[:id]).video
     current_user.unwatch(@video)
-    redirect_back fallback_location: root_path
+    # redirect_back fallback_location: root_path
   end
 end
