fe2d976c09f0a93bf0042281c038c1be2fc791a4
gittaiki <0215.taiki@gmail.com>
2022-01-30 10:24:17 +0900

    Change: youtubeのパラメータを変更#39

--- app/javascript/packs/videos/show.js
+++ app/javascript/packs/videos/show.js
@@ -3,31 +3,22 @@ document.addEventListener('DOMContentLoaded', () => {
   tag.src = "https://www.youtube.com/iframe_api";
   const firstScriptTag = document.getElementsByTagName('script')[0];
   firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-  let player;
   window.onYouTubeIframeAPIReady = function() {
-    player = new YT.Player('player', {
+    const player = new YT.Player('player', {
       width: '560',
       height: '315',
       videoId: gon.youtube_id,
-      events: {
-        // api呼び出す準備ができると起動
-        'onReady': function(evt) {
-          evt.target.mute();
-          evt.target.playVideo();
-        },
-        // プレーヤーの状態が変わると起動
-        'onStateChange': function(evt) {
-          switch (evt.data) {
-            // 動画をループ再生
-            case YT.PlayerState.ENDED:
-              evt.target.playVideo();
-              break;
-          }
-        }
-      },
       playerVars: {
         playsinline: 1,
-      }
+        rel: 0,
+      },
+      // events: {
+      //   // api呼び出す準備ができると起動
+      //   'onReady': function(evt) {
+      //     evt.target.mute();
+      //     evt.target.playVideo();
+      //   }
+      // }
     });
   }
 })
