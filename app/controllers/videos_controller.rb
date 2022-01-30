dc566c5b634ae47b40e6c5b43d2f78cf1bde77de
gittaiki <0215.taiki@gmail.com>
2022-01-30 19:34:09 +0900

    Add: 動画視聴状況を表示するプログレスバー設置#36

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -2,6 +2,9 @@ class VideosController < ApplicationController
   def index
     @q = Video.ransack(params[:q])
     @videos = @q.result(distinct: true).page(params[:page])
+    watch_videos = Watch.where(user_id: current_user.id).count.to_f
+    total_videos = Video.count.to_f
+    @progress_percent = watch_videos / total_videos * 100
   end
 
   def show
