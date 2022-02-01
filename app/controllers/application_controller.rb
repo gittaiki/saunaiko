380f6b838acc11fdeb5c94ef77f06fd9917c1ba9
gittaiki <0215.taiki@gmail.com>
2022-02-02 00:24:45 +0900

    Add: ヘッダーを設置#42

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :login_required
+  before_action :set_search
 
   private
 
@@ -13,5 +14,10 @@ class ApplicationController < ActionController::Base
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def set_search
+    @q = Video.ransack(params[:q])
+    @videos = @q.result(distinct: true).page(params[:page])
   end
 end
--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,10 +1,8 @@
 class VideosController < ApplicationController
   def index
-    @q = Video.ransack(params[:q])
-    @videos = @q.result(distinct: true).page(params[:page])
     watch_videos = Watch.where(user_id: current_user.id).count
     total_videos = Video.count
-    @progress_percent = watch_videos.to_f / total_videos.to_f * 100
+    @progress_percent = watch_videos.to_f / total_videos * 100
   end
 
   def show
