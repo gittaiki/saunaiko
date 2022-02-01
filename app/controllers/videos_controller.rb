7df66b8541f59dedeb576482d8ca4523995494a6
gittaiki <0215.taiki@gmail.com>
2022-02-01 22:27:57 +0900

    Fix: 少数第一位まで表示#36

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -2,9 +2,9 @@ class VideosController < ApplicationController
   def index
     @q = Video.ransack(params[:q])
     @videos = @q.result(distinct: true).page(params[:page])
-    watch_videos = Watch.where(user_id: current_user.id).count.to_f
-    total_videos = Video.count.to_f
-    @progress_percent = watch_videos / total_videos * 100
+    watch_videos = Watch.where(user_id: current_user.id).count
+    total_videos = Video.count
+    @progress_percent = watch_videos.to_f / total_videos.to_f * 100
   end
 
   def show
