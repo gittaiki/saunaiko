e88e1e8c5a9860130cb9f7e1583d490db6635fb3
gittaiki <0215.taiki@gmail.com>
2022-02-05 17:30:05 +0900

    Add: LINEの友だちをサウナに誘う機能を追加#49

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,10 +1,12 @@
 class VideosController < ApplicationController
+  # skip_before_action :login_required, only: %i[show]
   before_action :set_search
+  before_action :set_liff_id
 
   def index; end
 
   def show
     @video = Video.find(params[:id])
-    gon.youtube_id = @video.youtube_id
+    gon.video = @video
   end
 end
--- app/javascript/packs/videos/show.js
+++ app/javascript/packs/videos/show.js
@@ -7,7 +7,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const player = new YT.Player('player', {
       width: '560',
       height: '315',
-      videoId: gon.youtube_id,
+      videoId: gon.video.youtube_id,
       playerVars: {
         playsinline: 1,
         rel: 0,
@@ -21,4 +21,43 @@ document.addEventListener('DOMContentLoaded', () => {
       // }
     });
   }
+  const redirect_url = `https://liff.line.me/${gon.liff_id}/videos/${gon.video.id}`;
+  const send = document.getElementById('send');
+  send.addEventListener('click', () => {
+    // LIFFアプリを初期化。初期化するとSDKのメソッドを実行できる。
+    liff.init({
+      liffId: gon.liff_id
+    })
+      .then(() => {
+        if (!liff.isLoggedIn()) {
+          // 開発時、外部ブラウザからアクセスするために利用
+          liff.login()
+        }
+      });
+      liff.shareTargetPicker([
+        message = {
+          "type": "template",
+          "altText": "サウナのお誘いが届いています",
+          "template": {
+            "thumbnailImageUrl": "https://res.cloudinary.com/dr1peiwz2/image/upload/v1613642190/girl_ymjnoj.jpg",
+            "type": "buttons",
+            "title": "サウナのお誘い",
+            "text": "サウナのお誘いが届いています",
+            "actions": [
+              {
+                "type": "uri",
+                "label": "誘われたサウナを確認する",
+                "uri": redirect_url
+              }
+            ]
+          }
+        }
+      ])
+      .then(() => {
+        document.getElementById('success').innerText = 'サウナのお誘いをしました'
+      })
+      .catch(() => {
+        document.getElementById('error').innerText = 'サウナのお誘いに失敗しました'
+      });
+  })
 })
