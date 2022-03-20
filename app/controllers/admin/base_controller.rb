6f402a0b6a09c836550b587a95b8b05d9f2d62a0
gittaiki <0215.taiki@gmail.com>
2022-03-20 19:12:49 +0900

    Add: 管理画面に動画のCRUD機能追加

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,11 +1,12 @@
 class Admin::BaseController < ApplicationController
   before_action :check_admin
   layout 'admin/layouts/application'
+  add_flash_types :success, :danger
 
   private
 
   def check_admin
-    redirect_to root_path, warning: '権限がありません' unless current_user.admin?
+    redirect_to root_path unless current_user.admin?
   end
 
   def set_admin_liff_id
--- app/controllers/admin/videos_controller.rb
+++ app/controllers/admin/videos_controller.rb
@@ -1,18 +1,53 @@
 class Admin::VideosController < Admin::BaseController
+  before_action :set_video, only: %i[show edit update destroy]
+
   def index
     @q = Video.ransack(params[:q])
-    videos = @q.result(distinct: true)
+    videos = @q.result(distinct: true).order(created_at: :desc)
     @videos = Kaminari.paginate_array(videos).page(params[:page])
   end
 
   def show
-    @video = Video.find(params[:id])
     gon.video = @video
   end
 
   def new
+    @video = Video.new
   end
 
-  def edit
+  def create
+    @video = Video.new(video_params)
+    if @video.save
+      redirect_to admin_videos_path, success: '動画を追加しました'
+    else
+      flash.now[:danger] = '動画を追加できませんでした'
+      render :new
+    end
+  end
+
+  def edit; end
+
+  def update
+    if @video.update(video_params)
+      redirect_to admin_video_path(@video), success: '動画を更新しました'
+    else
+      flash.now[:danger] = '動画を更新できませんでした'
+      render :edit
+    end
+  end
+
+  def destroy
+    @video.destroy!
+    redirect_to admin_videos_path
+  end
+
+  private
+
+  def set_video
+    @video = Video.find(params[:id])
+  end
+
+  def video_params
+    params.require(:video).permit(:youtube_id, :address, :sauna)
   end
 end
