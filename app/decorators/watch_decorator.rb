92b017a061ca1ba4d92c681e90c85a8270a05592
gittaiki <0215.taiki@gmail.com>
2022-06-04 09:46:03 +0900

    Fix: デコレーターの導入#90

---
+++ app/decorators/watch_decorator.rb
@@ -0,0 +1,16 @@
+class WatchDecorator < Draper::Decorator
+  delegate_all
+
+  # Define presentation-specific methods here. Helpers are accessed through
+  # `helpers` (aka `h`). You can override attributes, for example:
+  #
+  #   def created_at
+  #     helpers.content_tag :span, class: 'time' do
+  #       object.created_at.strftime("%a %m/%d/%y")
+  #     end
+  #   end
+
+  def percent
+    count * 100 / Video.all.count
+  end
+end
--- app/models/watch.rb
+++ app/models/watch.rb
@@ -3,4 +3,8 @@ class Watch < ApplicationRecord
   belongs_to :video
 
   validates :user_id, uniqueness: { scope: :video_id }
+
+  def self.total(user_id)
+    where(user_id: user_id)
+  end
 end
