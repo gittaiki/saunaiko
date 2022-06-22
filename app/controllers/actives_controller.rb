f5f752ce2f9ad2d124a93eac5c96c4f803c9410a
gittaiki <0215.taiki@gmail.com>
2022-06-22 22:51:53 +0900

    Fix: プロフィールをサ活に変更#98

--- app/controllers/actives_controller.rb
+++ app/controllers/actives_controller.rb
@@ -1,8 +1,10 @@
 class ActivesController < ApplicationController
-  skip_before_action :login_required, only: %i[show]
+  # skip_before_action :login_required, only: %i[show]
   before_action :set_search
+  before_action :set_liff_id, only: %i[show]
 
-  def index
+  def show
+    @views = WatchDecorator.decorate(Watch.total(current_user.id))
     @actives = current_user.actives
     @month = params[:month] ? Date.parse(params[:month]) : Time.zone.today
     @actives_chart = @actives.visited_month(@month)
@@ -14,56 +16,9 @@ class ActivesController < ApplicationController
 
   def create
     @active = current_user.actives.build(active_params)
-    if @active.sauna_minute >= 6
-      @active.score = case @active.sauna_minute
-                      when 6
-                        @active.score + 1
-                      when 7
-                        @active.score + 2
-                      when 8
-                        @active.score + 3
-                      when 9
-                        @active.score + 4
-                      when 10
-                        @active.score + 6
-                      when 11
-                        @active.score + 8
-                      else
-                        @active.score + 10
-                      end
-    end
-    if @active.water_bath_minute >= 1
-      @active.score = case @active.water_bath_minute
-                      when 1
-                        @active.score + 7
-                      else
-                        @active.score + 10
-                      end
-    end
-    if @active.break_minute >= 5
-      @active.score = case @active.break_minute
-                      when 5
-                        @active.score + 1
-                      when 6
-                        @active.score + 3
-                      when 7
-                        @active.score + 5
-                      when 8
-                        @active.score + 7
-                      when 9
-                        @active.score + 9
-                      else
-                        @active.score + 10
-                      end
-    end
-    if @active.cycle_count >= 2
-      @active.score = case @active.water_bath_minute
-                      when 2
-                        @active.score + 5
-                      else
-                        @active.score + 10
-                      end
-    end
+    sauna_score if @active.sauna_minute >= 6
+    water_bath_score if @active.water_bath_minute >= 1
+    cycle_score if @active.cycle_count >= 2
     if @active.save
       redirect_to actives_path, success: 'サ活を記録しました'
     else
@@ -72,14 +27,39 @@ class ActivesController < ApplicationController
     end
   end
 
-  def show
-    @active = Active.find(params[:id])
-  end
-
   private
 
   def active_params
-    params.require(:active).permit(:visited_on, :sauna_minute, :water_bath_minute, :break_minute,
-                                   :cycle_count).merge(score: 60)
+    params.require(:active).permit(:visited_on, :sauna_minute, :water_bath_minute,
+                                   :break_minute, :cycle_count).merge(score: 60)
+  end
+
+  def sauna_score
+    @active.score = case @active.sauna_minute
+                    when 6..8
+                      @active.score + 4
+                    when 9..11
+                      @active.score + 8
+                    else
+                      @active.score + 10
+                    end
+  end
+
+  def water_bath_score
+    @active.score = case @active.water_bath_minute
+                    when 1
+                      @active.score + 7
+                    else
+                      @active.score + 10
+                    end
+  end
+
+  def cycle_score
+    @active.score = case @active.cycle_count
+                    when 2
+                      @active.score + 10
+                    else
+                      @active.score + 20
+                    end
   end
 end
--- app/controllers/profiles_controller.rb
+++
@@ -1,8 +0,0 @@
-class ProfilesController < ApplicationController
-  before_action :set_search
-  before_action :set_liff_id
-
-  def show
-    @views = WatchDecorator.decorate(Watch.total(current_user.id))
-  end
-end
